a2a2d0cea70c2f99ecd529de3273b315ea7e794e
donnymays <dtmays2@gmail.com>
2020-10-26 11:39:49 -0700

    debug resue_from methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::API
   include Response
+  include Excepcion
+  
+  # rescue_from ActiveRecord::RecordNotFound do |exception|
+  #   json_response({ message: exception.message }, :not_found)
+  # end
+
+  # rescue_from ActiveRecord::RecordInvalid do |exception|
+  #   json_response({ message: exception.message }, 422)
+  # end
+
+  # rescue_from TypeError do |exception|
+  #   json_response( {  message:exception.message }, 500)
+  # end
+
 end
