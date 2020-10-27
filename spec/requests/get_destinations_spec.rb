89e6cd92603b208abfacb7126adef9a50001ecb9
donnymays <dtmays2@gmail.com>
2020-10-27 11:45:16 -0700

    successful random review spec

--- spec/requests/get_destinations_spec.rb
+++ spec/requests/get_destinations_spec.rb
@@ -40,3 +40,22 @@ describe "get highest rated destination", :type => :request do
       expect(JSON.parse(response.body)[0]['content']).to eq('Rad beach town')
     end
   end
+
+
+  describe "get random desitination", :type => :request do
+    let!(:reviews) {
+      [ FactoryBot.create(:review, content: 'Rad beach town', city: 'Virginia Beach', country: 'United States', user_name: 'vstew', rating: 5), FactoryBot.create( :review, content: 'Really rad beach town', city: 'Norfolk', country: 'United States', user_name: 'vstew', rating: 4), FactoryBot.create( :review, content: 'Rad old town', city: 'Colonial Williamsburg', country: 'United States', user_name: 'vstew', rating: 3) ]
+    }
+    
+    before { get '/destination/random' }
+  
+      # Test for header
+      it 'returns status code 200' do
+        expect(response).to have_http_status(:success)
+      end
+    
+      # Test that Random returns 1 review object
+      it 'returns the objects that have the highest rating' do
+        expect(JSON.parse(response.body).size).to eq(1)
+      end
+  end
