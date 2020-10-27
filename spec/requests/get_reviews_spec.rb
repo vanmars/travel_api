4b99852c974178c012b763e546f420f6780733f0
donnymays <dtmays2@gmail.com>
2020-10-27 09:59:42 -0700

    passing get city search parameter specs

--- spec/requests/get_reviews_spec.rb
+++ spec/requests/get_reviews_spec.rb
@@ -16,3 +16,27 @@ describe 'get all reviews route', :type => :request do
   end
 
 end
+
+
+
+# Test the City Search Parameter
+describe 'get all reviews matching city search', :type => :request do
+  let!(:reviews) { FactoryBot.create(:review, content: 'Rad beach town', city: 'Virginia Beach', country: 'United States', user_name: 'vstew', rating: 5) }
+
+  before { get '/reviews?city=Virginia Beach' }
+
+  # Test for header
+  it 'returns status code 200' do
+    expect(response).to have_http_status(:success)
+  end
+
+  # Test for body size
+  it 'returns all reviews matching the search' do
+    expect(JSON.parse(response.body).size).to eq(1)
+  end
+
+  # Test the body content
+  it 'returns the correct content' do
+    expect(JSON.parse(response.body).first['city']).to eq('Virginia Beach')
+  end
+end
